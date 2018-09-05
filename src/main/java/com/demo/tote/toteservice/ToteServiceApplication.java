12666a894f3c9e9e1810eddd2a826b9fbc3885cd
Himani Sharma <himanis993@gmail.com>
2018-09-05 03:09:04 -0400

    Initial commit

---
+++ src/main/java/com/demo/tote/toteservice/ToteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.demo.tote.toteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToteServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/demo/tote/toteservice/ToteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.tote.toteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToteServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
